db6db13e459ab448e5a04f1b9f2e986dbca0de8a
Eclipium <66476312+eclipium@users.noreply.github.com>
2021-01-01 11:16:02 +0100

    Update playstore.js

--- commands/playstore.js
+++ commands/playstore.js
@@ -1,5 +1,24 @@
 exports.run = async (client, message, args) => {
   var Discord = require("discord.js");
   var gplay = require("google-play-scraper");
-  message.channel.send("Cette commande est actuellement en maintenance");
+  let recherche = args.join(" ");
+  if (!recherche) return message.reply("vous n'avez demandé aucune recherche.");
+  var attente = new Discord.MessageEmbed()
+    .setDescription("Recherche en cours")
+    .setColor("2f3136");
+    
+  message.channel.send(attente).then((m) => {
+    gplay.app({ appId: args, lang:'fr' }).then((App) => {
+         message.channel.send(new Discord.MessageEmbed()
+    .setDescription("**Nom : **"+App.title+"\n **Développeur : **"+App.developer+"\n **Version : **"+App.version+"\n **Description : **"+App.description
+                   +"\n **Installations : **"+App.installs+"\n **Score : **"+App.score+"\n **Taille : **"+App.size)
+    .setColor("2f3136"));
+        }, () => {
+            message.channel.send(new Discord.MessageEmbed()
+    .setDescription(args+" Application non trouvée")
+    .setColor("2f3136"));
+
+        });
+          
+  });
 };
