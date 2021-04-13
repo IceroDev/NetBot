9f5ef08769c7b5dd5b788d88b829d7b569fb3ec9
Jean Staffe <69088819+IceroDev@users.noreply.github.com>
2021-04-13 10:11:03 +0200

    Delete commands directory

--- commands/quelestlemeilleurhebergeur.js
+++
@@ -1,7 +0,0 @@
-exports.run = (client, message, args) => {
-  var Discord = require("discord.js");
-
-  message.channel.send(
-    "NetHeberg.fr évidemment, la question ne se pose même pas"
-  );
-};
